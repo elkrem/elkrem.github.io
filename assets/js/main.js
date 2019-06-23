57732b24bccb1e4512ec929ca3d02633f41831e7
Islam El-Ashi <ielashi@gmail.com>
2019-06-23 10:22:39 +0200

    Remove Swarm from intro header.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
     new Typewriter('#typewriter', {
-      strings: ['blockchain.', 'Ethereum.', 'Bitcoin.', 'IPFS.', 'Whisper.', 'Swarm.'],
+      strings: ['blockchain.', 'Ethereum.', 'Bitcoin.', 'IPFS.', 'Whisper.'],
       autoStart: true,
       loop: true,
     });
   ;
     new Typewriter('#typewriter2', {
-      strings: ['blockchain.', 'Ethereum.', 'Bitcoin.', 'IPFS.', 'Whisper.', 'Swarm.'],
+      strings: ['blockchain.', 'Ethereum.', 'Bitcoin.', 'IPFS.', 'Whisper.'],
       autoStart: true,
       loop: true,
     });
